735ee7bc3dd5d4df89ce66c2971134534573751d
Jordan Reger <jordanreger@gmail.com>
2022-03-09 17:39:26 -0500

    mobile should be responsive now

--- main.ts
+++ main.ts
@@ -18,131 +18,137 @@ async function handler(req: Request): Promise<Response> {
     case route('/info/.'):
       const mdPath = path.replace("/info/", "").replace(".md", "");
       tr = true, rb = `
-      <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Archivo:wght@700&family=Inter&display=swap" rel="stylesheet">
-      <style>
-        @media only screen and (min-width: 600px) {
-          html {
-            padding-left: 2rem;
-            padding-right: 2rem;
-            padding-bottom: 2rem;
-          }
-          body {
-            background: #101010;
-            text-align: justify;
-            font-size: 1.25rem;
-          }
-          h1 {
-            color: #f0f0f0;
-            font-family: Archivo;
-            font-size: 4rem;
+      <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@700&family=Inter&display=swap" rel="stylesheet">
+        <style>
+          @media only screen and (min-width: 600px) {
+            html {
+              padding-left: 2rem;
+              padding-right: 2rem;
+              padding-bottom: 2rem;
+            }
+            body {
+              background: #101010;
+              text-align: justify;
+              font-size: 1.25rem;
+            }
+            h1 {
+              color: #f0f0f0;
+              font-family: Archivo;
+              font-size: 4rem;
+            }
+
+            h2 {
+              color: #e0e0e0;
+              font-family: Archivo;
+              font-size: 3rem;
+            }
+
+            h3 {
+              color: #d0d0d0;
+              font-family: Archivo;
+              font-size: 2rem;
+            }
+
+            h4, h5, h6 {
+              color: #c4c4c4;
+              font-family: Archivo;
+            }
+
+            p, ul, ol, table, body {
+              color: #c4c4c4;
+              font-family: Inter;
+            }
+
+            table {
+              border-color: #212121;
+            }
+
+            blockquote {
+              border-left: 2px solid #212121;
+              margin: 2px 10px;
+              padding: 2px 10px;
+              font-family: Inter;
+            }
+
+            code {
+              color: #e0e0e0;
+            }
+
+            a { 
+              color: #FF9A51;
+            }
           }
 
-          h2 {
-            color: #e0e0e0;
-            font-family: Archivo;
-            font-size: 3rem;
+          @media only screen and (max-width: 600px) {
+            html {
+              padding-left: 2rem;
+              padding-right: 2rem;
+              padding-bottom: 2rem;
+            }
+            body {
+              background: #101010;
+              text-align: justify;
+              font-size: 1.5rem;
+            }
+            h1 {
+              color: #f0f0f0;
+              font-family: Archivo;
+              font-size: 3rem;
+            }
+    
+            h2 {
+              color: #e0e0e0;
+              font-family: Archivo;
+              font-size: 2.5rem;
+            }
+    
+            h3 {
+              color: #d0d0d0;
+              font-family: Archivo;
+              font-size: 1.75rem;
+            }
+    
+            h4, h5, h6 {
+              color: #c4c4c4;
+              font-family: Archivo;
+            }
+    
+            p, ul, ol, table, body {
+              color: #c4c4c4;
+              font-family: Inter;
+              font-size: 1.5rem;
+            }
+    
+            table {
+              border-color: #212121;
+            }
+    
+            blockquote {
+              border-left: 2px solid #212121;
+              margin: 2px 10px;
+              padding: 2px 10px;
+              font-family: Inter;
+              font-size: 1.5rem;
+            }
+    
+            code {
+              color: #e0e0e0;
+              font-size: 1.5rem;
+            }
+    
+            a { 
+              color: #FF9A51;
+            }
           }
-
-          h3 {
-            color: #d0d0d0;
-            font-family: Archivo;
-            font-size: 2rem;
-          }
-
-          h4, h5, h6 {
-            color: #c4c4c4;
-            font-family: Archivo;
-          }
-
-          p, ul, ol, table, body {
-            color: #c4c4c4;
-            font-family: Inter;
-          }
-
-          table {
-            border-color: #212121;
-          }
-
-          blockquote {
-            border-left: 2px solid #212121;
-            margin: 2px 10px;
-            padding: 2px 10px;
-            font-family: Inter;
-          }
-
-          code {
-            color: #e0e0e0;
-          }
-
-          a { 
-            color: #FF9A51;
-          }
-        }
-
-        @media only screen and (max-width: 600px) {
-          html {
-            padding-left: 2rem;
-            padding-right: 2rem;
-            padding-bottom: 2rem;
-          }
-          body {
-            background: #101010;
-            text-align: justify;
-            font-size: 3rem;
-          }
-          h1 {
-            color: #f0f0f0;
-            font-family: Archivo;
-            font-size: 6rem;
-          }
-  
-          h2 {
-            color: #e0e0e0;
-            font-family: Archivo;
-            font-size: 5rem;
-          }
-  
-          h3 {
-            color: #d0d0d0;
-            font-family: Archivo;
-            font-size: 4rem;
-          }
-  
-          h4, h5, h6 {
-            color: #c4c4c4;
-            font-family: Archivo;
-          }
-  
-          p, ul, ol, table, body {
-            color: #c4c4c4;
-            font-family: Inter;
-            font-size: 3rem;
-          }
-  
-          table {
-            border-color: #212121;
-          }
-  
-          blockquote {
-            border-left: 2px solid #212121;
-            margin: 2px 10px;
-            padding: 2px 10px;
-            font-family: Inter;
-          }
-  
-          code {
-            color: #e0e0e0;
-            font-size: 2rem;
-          }
-  
-          a { 
-            color: #FF9A51;
-          }
-        }
-      </style>
-      ${Marked.parse(await file(`./md/${mdPath}.md`)).content}
+        </style>
+      </head>
+      <body>
+        ${Marked.parse(await file(`./md/${mdPath}.md`)).content}
+      </body>
       `, ct = "text/html; charset=UTF-8";
       break;
 
