70492573c4432a9a42f903702dc969165b3ecd40
Jordan Reger <jordanreger@gmail.com>
2022-03-10 21:38:43 -0500

    v0.1~WP1

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 // deno-lint-ignore-file no-case-declarations
-import { serve, ConnInfo } from "https://deno.land/std@0.128.0/http/server.ts";
-import { Marked } from "https://deno.land/x/markdown@v2.0.0/mod.ts";
+import { serve, ConnInfo } from "https://deno.land/std@0.128.0/http/server.ts"
 import { createReporter } from "https://deno.land/x/g_a@0.1.2/mod.ts";
-
 const ga = createReporter();
+
+import { md } from "./src/markdown.ts";
 
 async function handler(req: Request, conn: ConnInfo): Promise<Response> {
   const url = new URL(req.url);
@@ -14,145 +14,25 @@ async function handler(req: Request, conn: ConnInfo): Promise<Response> {
   let tr, rb, ct = "";
 
   switch(path){
+    // paths
     case '/':
       tr = true, rb = await file("./src/index.html"), ct = "text/html; charset=UTF-8";
       break;
-    
-    case route('/info/.'):
-      const mdPath = path.replace("/info/", "").replace(".md", "");
-      tr = true, rb = `
-      <head>
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@700&family=Inter&display=swap" rel="stylesheet">
-        <style>
-          @media only screen and (min-width: 600px) {
-            html {
-              padding-left: 2rem;
-              padding-right: 2rem;
-              padding-bottom: 2rem;
-            }
-            body {
-              background: #101010;
-              text-align: justify;
-              font-size: 1.25rem;
-            }
-            h1 {
-              color: #f0f0f0;
-              font-family: Archivo;
-              font-size: 4rem;
-            }
 
-            h2 {
-              color: #e0e0e0;
-              font-family: Archivo;
-              font-size: 3rem;
-            }
+    case '/wiki':
+      tr = true, rb = "wiki", ct = "text/html; charset=UTF-8";
+      break;
+    case route('/wiki/.'):
+      const mdPath = path.replace("/docs/", "").replace(".md", "");
+      tr = true, rb = md(mdPath), ct = "text/html; charset=UTF-8";
+      break;
 
-            h3 {
-              color: #d0d0d0;
-              font-family: Archivo;
-              font-size: 2rem;
-            }
-
-            h4, h5, h6 {
-              color: #c4c4c4;
-              font-family: Archivo;
-            }
-
-            p, ul, ol, table, body {
-              color: #c4c4c4;
-              font-family: Inter;
-            }
-
-            table {
-              border-color: #212121;
-            }
-
-            blockquote {
-              border-left: 2px solid #212121;
-              margin: 2px 10px;
-              padding: 2px 10px;
-              font-family: Inter;
-            }
-
-            code {
-              color: #e0e0e0;
-            }
-
-            a { 
-              color: #FF9A51;
-            }
-          }
-
-          @media only screen and (max-width: 600px) {
-            html {
-              padding-left: 0.75rem;
-              padding-right: 0.75rem;
-              padding-bottom: 0.75rem;
-            }
-            body {
-              background: #101010;
-              text-align: justify;
-              font-size: 1.25rem;
-            }
-            h1 {
-              color: #f0f0f0;
-              font-family: Archivo;
-              font-size: 3rem;
-            }
-    
-            h2 {
-              color: #e0e0e0;
-              font-family: Archivo;
-              font-size: 2.5rem;
-            }
-    
-            h3 {
-              color: #d0d0d0;
-              font-family: Archivo;
-              font-size: 1.75rem;
-            }
-    
-            h4, h5, h6 {
-              color: #c4c4c4;
-              font-family: Archivo;
-            }
-    
-            p, ul, ol, table, body {
-              color: #c4c4c4;
-              font-family: Inter;
-              font-size: 1.25rem;
-            }
-    
-            table {
-              border-color: #212121;
-            }
-    
-            blockquote {
-              border-left: 2px solid #212121;
-              margin: 2px 10px;
-              padding: 2px 10px;
-              font-family: Inter;
-              font-size: 1.25rem;
-            }
-    
-            code {
-              color: #e0e0e0;
-              font-size: 1.25rem;
-            }
-    
-            a { 
-              color: #FF9A51;
-            }
-          }
-        </style>
-      </head>
-      <body>
-        ${Marked.parse(await file(`./md/${mdPath}.md`)).content}
-      </body>
-      `, ct = "text/html; charset=UTF-8";
+    // bin
+    case '/logo.svg':
+      tr = true, rb = await file("./bin/logo.svg"), ct = "image/svg+xml";
+      break;
+    case '/menu.svg':
+      tr = true, rb = await file("./bin/menu.svg"), ct = "image/svg+xml";
       break;
 
     default:
@@ -162,10 +42,10 @@ async function handler(req: Request, conn: ConnInfo): Promise<Response> {
   let res;
 
   if(tr){
-    res = new Response(rb, { headers: { "content-type": ct } });
+    res = new Response(await rb, { headers: { "content-type": ct } });
     ga(req, conn, res!, performance.now());
   } else {
-    res = Response.redirect(rb, 302);
+    res = Response.redirect(await rb, 302);
   }
   return res!;
 }
---
+++ src/markdown.ts
@@ -0,0 +1,139 @@
+import { Marked } from "https://deno.land/x/markdown@v2.0.0/mod.ts";
+
+const file = async (fp:string) => { const d = new TextDecoder("utf-8"); return d.decode(await Deno.readFile(fp))}
+
+export const md = async (src: string) => {
+    return `
+    <head>
+      <meta name="viewport" content="width=device-width,initial-scale=1">
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+      <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@700&family=Inter&display=swap" rel="stylesheet">
+      <style>
+        @media only screen and (min-width: 600px) {
+          html {
+            padding-left: 2rem;
+            padding-right: 2rem;
+            padding-bottom: 2rem;
+          }
+          body {
+            background: #101010;
+            text-align: justify;
+            font-size: 1.25rem;
+          }
+          h1 {
+            color: #f0f0f0;
+            font-family: Archivo;
+            font-size: 4rem;
+          }
+
+          h2 {
+            color: #e0e0e0;
+            font-family: Archivo;
+            font-size: 3rem;
+          }
+
+          h3 {
+            color: #d0d0d0;
+            font-family: Archivo;
+            font-size: 2rem;
+          }
+
+          h4, h5, h6 {
+            color: #c4c4c4;
+            font-family: Archivo;
+          }
+
+          p, ul, ol, table, body {
+            color: #c4c4c4;
+            font-family: Inter;
+          }
+
+          table {
+            border-color: #212121;
+          }
+
+          blockquote {
+            border-left: 2px solid #212121;
+            margin: 2px 10px;
+            padding: 2px 10px;
+            font-family: Inter;
+          }
+
+          code {
+            color: #e0e0e0;
+          }
+
+          a { 
+            color: #FF9A51;
+          }
+        }
+
+        @media only screen and (max-width: 600px) {
+          html {
+            padding-left: 0.75rem;
+            padding-right: 0.75rem;
+            padding-bottom: 0.75rem;
+          }
+          body {
+            background: #101010;
+            text-align: justify;
+            font-size: 1.25rem;
+          }
+          h1 {
+            color: #f0f0f0;
+            font-family: Archivo;
+            font-size: 3rem;
+          }
+  
+          h2 {
+            color: #e0e0e0;
+            font-family: Archivo;
+            font-size: 2.5rem;
+          }
+  
+          h3 {
+            color: #d0d0d0;
+            font-family: Archivo;
+            font-size: 1.75rem;
+          }
+  
+          h4, h5, h6 {
+            color: #c4c4c4;
+            font-family: Archivo;
+          }
+  
+          p, ul, ol, table, body {
+            color: #c4c4c4;
+            font-family: Inter;
+            font-size: 1.25rem;
+          }
+  
+          table {
+            border-color: #212121;
+          }
+  
+          blockquote {
+            border-left: 2px solid #212121;
+            margin: 2px 10px;
+            padding: 2px 10px;
+            font-family: Inter;
+            font-size: 1.25rem;
+          }
+  
+          code {
+            color: #e0e0e0;
+            font-size: 1.25rem;
+          }
+  
+          a { 
+            color: #FF9A51;
+          }
+        }
+      </style>
+    </head>
+    <body>
+      ${Marked.parse(await file(`./wiki/${src}.md`)).content}
+    </body>
+    `;
+}
