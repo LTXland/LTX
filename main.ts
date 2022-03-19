f75382d8efbb5488983a703de4624e286ce086c7
Jordan Reger <jordanreger@gmail.com>
2022-03-19 13:58:11 -0400

    [core/main] remove GA and add menu component

--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
 // deno-lint-ignore-file no-case-declarations
-import { serve, ConnInfo } from "https://deno.land/std@0.128.0/http/server.ts"
-import { createReporter } from "https://deno.land/x/g_a@0.1.2/mod.ts";
-const ga = createReporter();
+import { serve } from "https://deno.land/std@0.128.0/http/server.ts"
 
 import { md } from "./src/markdown.ts";
 
-async function handler(req: Request, conn: ConnInfo): Promise<Response> {
+async function handler(req: Request): Promise<Response> {
   const url = new URL(req.url);
   const path = url.pathname, _params = new URLSearchParams(url.search);
   const route = (route:string) => { const regexRoute = new RegExp(route, "gmi"); if(regexRoute.test(path)){ return path } else { return "/404" }}
@@ -35,12 +33,20 @@ async function handler(req: Request, conn: ConnInfo): Promise<Response> {
       tr = true, rb = md(mdPath), ct = "text/html; charset=UTF-8";
       break;
 
+    // components
+    case '/src/components/menu.mjs':
+      tr = true, rb = await file("./src/components/menu.mjs"), ct = "text/javascript";
+      break;
+
     // bin
     case '/bin/logo.svg':
       tr = true, rb = await file("./bin/logo.svg"), ct = "image/svg+xml";
       break;
     case '/bin/menu.svg':
       tr = true, rb = await file("./bin/menu.svg"), ct = "image/svg+xml";
+      break;
+    case '/bin/more_vert_white_48dp.svg':
+      tr = true, rb = await file("./bin/more_vert_white_48dp.svg"), ct = "image/svg+xml";
       break;
     case '/bin/starship-diagram-back.svg':
       tr = true, rb = await file("./bin/starship-diagram-back.svg"), ct = "image/svg+xml";
@@ -57,7 +63,6 @@ async function handler(req: Request, conn: ConnInfo): Promise<Response> {
 
   if(tr){
     res = new Response(await rb, { headers: { "content-type": ct } });
-    ga(req, conn, res!, performance.now());
   } else {
     res = Response.redirect(await rb, 302);
   }
