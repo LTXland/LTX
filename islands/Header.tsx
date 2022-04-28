66fc4bb6c12cc336ebcd13d704d354ef6cba1e79
Jordan Reger <jordanreger@gmail.com>
2022-04-28 13:09:44 -0400

    chore: minor css fixes

--- islands/Header.tsx
+++ islands/Header.tsx
@@ -1,62 +1,40 @@
 /** @jsx h */
 
-import { h, useState } from "../client_deps.ts";
+import { h, Head } from "../client_deps.ts";
 
 export default function Header(props: { active: string }) {
-  const [open, setOpen] = useState(false);
-
   const items = [
     {
-      name: "Home",
+      name: "HOME",
       href: "/",
     },
     {
-      name: "Launches",
+      name: "LAUNCHES",
       href: "/launches",
     },
     {
-      name: "Streams",
+      name: "STREAMS",
       href: "/streams",
     },
     {
-      name: "Wiki",
-      href: "/wiki",
-    },
-    {
-      name: "Github",
-      href: "https://github.com/LTXland/LTX",
+      name: "GITHUB ↗️",
+      href: "https://github.com/LTXland",
     },
   ];
 
   return (
-    <header>
-      <a href="/">
-        <img id="logo" src="/logo.svg" alt="LTX logo" draggable={false} />
-      </a>
-      <div id="links">
+    <div class="header">
+      <Head>
+        <link rel="stylesheet" href="./index.css" />
+      </Head>
+      <img src="./logo.svg" alt="LTX" class="logo" />
+      <div class="links">
         {items.map((item) => (
-          <div>
-            <a href={item.href} class={props.active == item.href ? "active" : ""}>
-              {item.name}
-            </a>
-          </div>
+          <a href={item.href} class={props.active == item.href ? "active" : ""}>
+            {item.name}
+          </a>
         ))}
       </div>
-      <div class="menu-toggle" onClick={() => setOpen(!open)}>
-        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>
-      </div>
-      {open && (
-      <div id="menu-content">
-        <div id="menu-links">
-          {items.map((item) => (
-            <div>
-              <a href={item.href} class={props.active == item.href ? "active" : ""}>
-                {item.name}
-              </a>
-            </div>
-          ))}
-        </div>
-      </div>)}
-    </header>
+    </div>
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,5 +1,6 @@
 /** @jsx h */
 import { h, Head } from "../client_deps.ts";
+import Header from "../islands/Header.tsx";
 
 export default function Home() {
   return (
@@ -8,17 +9,10 @@ export default function Home() {
         <title>LTX — Your one stop shop for everything space.</title>
         <meta name="title" content="LTX — Your one stop shop for everything space."/>
         <meta name="description" content="Built by the community for the community on GitHub, LTX is the first open source rocket launch coverage website. Join the community on Discord to get involved with launch parties, chat with other space fans, and coordinate development with fellow developers."/>
-
         <link rel="stylesheet" href="./index.css" />
       </Head>
       <div class="header">
-        <img src="./logo.svg" alt="LTX" class="logo" />
-        <div class="links">
-          <a href="/">HOME</a>
-          <a href="/launches">LAUNCHES</a>
-          <a href="/streams">STREAMS</a>
-          <a href="https://github.com/LTXland">GITHUB <span class="arrow">🡥</span></a>
-        </div>
+        <Header active="/"></Header>
       </div>
       <div class="x">
         <div class="box">
